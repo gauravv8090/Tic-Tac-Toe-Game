969c7850dcc73667d4a12ebed2f39f85f149b2dc
gauravv8090 <gaurav8090983461@gmail.com>
2023-01-02 16:09:52 +0530

    structure created

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Square from './components/Square';
+import React, {useState} from "react";
+
+const initialState = ["","","","","","","","","" ];
 
 function App() {
+
+  const [ state, setState ]  = useState( initialState );
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <p className='Heading' >Tic Tac Toe </p>
+      <div className='row justify-center' >
+        <Square className='border-right-bottom' state={state[0]} />
+        <Square className='border-right-bottom' state={state[1]}/>
+        <Square className='border-bottom' state={state[2]}/>
+      </div>
+      <div className='row justify-center' >
+      <Square className='border-right-bottom' state={state[3]} />
+      <Square className='border-right-bottom' state={state[4]}/>
+      <Square className='border-bottom' state={state[5]}/>
+      </div>
+      <div className='row justify-center' >
+      <Square className='border-right' state={state[6]}/>
+      <Square className='border-right' state={state[7]}/>
+      <Square  state={state[8]}/>
+      </div>
+      <button className='button-style' > New game </button>
     </div>
   );
 }
---
+++ game-app/src/components/Square.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Square( props ) {
+
+    const classes = props.className?`${props.className} square ` : 'square';
+
+  return (
+    <span className={classes} >
+        {props.state}
+        </span>
+  );
+}
+
+export default Square;
